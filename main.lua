a795eed43eea86a0e6d556ce6eafc21845eb42d4
Ahmad El-baba <aelbaba33@outlook.com>
2023-07-04 13:20:36 -0400

    yay we have tiles just need map

--- main.lua
+++ main.lua
@@ -7,13 +7,10 @@ end
 
 function love.update(dt)
   if love.keyboard.isDown("up") then
-    w = w + 1
-    h = h + 1
+
   end
 end
 
 function love.draw()
-  -- love.graphics.setColor(0, 0.4, 0.4)
-  -- love.graphics.rectangle("fill", x, y, w, h)
   map_generator.Generate()
 end
--- src/MapGenerator.lua
+++ src/MapGenerator.lua
@@ -1,10 +1,28 @@
+local tile_size = 32
+
+local function tile_sheet_to_tile_map(image)
+    local tile_map = {}
+    for i = 0, (image:getWidth() / tile_size) - 1, 1 do
+        for j = 0, (image:getHeight() / tile_size) - 1, 1 do
+            table.insert(tile_map,
+                love.graphics.newQuad(j * 32, i * 32, tile_size, tile_size, image:getWidth(), image:getHeight()))
+        end
+    end
+    return tile_map
+end
+
 return function(image_path)
     local MapGenerator = {}
-    local Image = love.graphics.newImage(image_path)
-    local top_left = love.graphics.newQuad(0, 0, 16, 16, Image:getWidth(), Image:getHeight())
-
+    local image = love.graphics.newImage(image_path)
+    local tile_map = tile_sheet_to_tile_map(image)
     function MapGenerator:Generate()
-        love.graphics.draw(Image, top_left, 0, 0)
+        love.graphics.push()
+        love.graphics.scale(4, 4)
+        love.graphics.draw(image, tile_map[1], 0, 0)
+        love.graphics.draw(image, tile_map[2], tile_size, 0)
+        love.graphics.draw(image, tile_map[3], tile_size * 2, 0)
+        love.graphics.draw(image, tile_map[3], tile_size * 3, 0)
+        love.graphics.pop()
     end
 
     return MapGenerator
