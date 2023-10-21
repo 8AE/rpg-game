83c15e75d0e58e619de3b66e1f73fdbb110d326b
Ahmad El-baba <aelbaba33@outlook.com>
2023-10-21 17:09:55 -0400

    better scaling text

--- src/constants.lua
+++ src/constants.lua
@@ -3,5 +3,6 @@ return {
   legacy_offset = 1,
   zoom = 2,
   base_map_path = 'data/map/',
-  base_event_path = 'data/event/'
+  base_event_path = 'data/event/',
+  small_text_scale_factor = .5,
 }
--- src/font/rpg_print.lua
+++ src/font/rpg_print.lua
@@ -1,9 +1,10 @@
 local font = require('src.font.font')
 
-return function(text, x, y)
+return function(text, x, y, scale_factor)
+  scale_factor = scale_factor or 1
   love.graphics.push()
   love.graphics.setFont(font)
-  love.graphics.print(string.upper(text), x, y)
+  love.graphics.print(string.upper(text), x, y, 0, scale_factor, scale_factor)
   love.graphics.reset()
   love.graphics.pop()
 end
